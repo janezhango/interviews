bae9c57b30d4d732569c681fd16ab3f937f2b39a
janezhango <jane.110511@gmail.com>
2016-11-07 14:36:11 +0800

    Internal: add java8 for test

---
+++ java8/src/main/java/GenericTest.java
@@ -0,0 +1,18 @@
+/**
+ * Created by xiaojing on 11/7/2016.
+ */
+public class GenericTest<T> {
+    private T test;
+
+    public GenericTest(T test){
+        this.test = test;
+    }
+    public void print(){
+        System.out.println(test.toString());
+    }
+
+    public static void main(String[] args){
+        GenericTest<String> myGenerics = new GenericTest<>("test");
+        myGenerics.print();
+    }
+}
---
+++ java8/src/main/java/LambdaExpression.java
@@ -0,0 +1,23 @@
+/**
+ * Created by xiaojing on 11/7/2016.
+ */
+public class LambdaExpression {
+    interface IntegerMath {
+        int operation(int a, int b);
+    }
+
+    public int operateBinary(int a, int b, IntegerMath op) {
+        return op.operation(a, b);
+    }
+
+    public static void main(String... args) {
+
+        LambdaExpression myApp = new LambdaExpression();
+        IntegerMath addition = (a, b) -> a + b;
+        IntegerMath subtraction = (a, b) -> a - b;
+        System.out.println("40 + 2 = " +
+                myApp.operateBinary(40, 2, addition));
+        System.out.println("20 - 10 = " +
+                myApp.operateBinary(20, 10, subtraction));
+    }
+}
