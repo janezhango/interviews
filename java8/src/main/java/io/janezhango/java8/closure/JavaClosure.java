ecf230f02d8331c236374a43c82a0bad59eff4f9
janezhango <jane.110511@gmail.com>
2017-04-14 01:23:22 +0800

    add java8 lambda examples

---
+++ java8/src/main/java/io/janezhango/java8/closure/JavaClosure.java
@@ -0,0 +1,7 @@
+package io.janezhango.java8.closure;
+
+/**
+ * Created by xiaojing on 17/4/14.
+ */
+public class JavaClosure {
+}
---
+++ java8/src/main/java/io/janezhango/java8/generic/Main.java
@@ -0,0 +1,7 @@
+package io.janezhango.java8.generic;
+
+/**
+ * Created by xiaojing on 17/4/14.
+ */
+public class Main {
+}
